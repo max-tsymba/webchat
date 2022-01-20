71cfed5e95ceda1a9056687b174a19b8aff7b808
max-tsymba <young.bezero@gmail.com>
2022-01-20 13:06:58 +0200

    search bar

--- client/src/components/Searchbar/index.tsx
+++ client/src/components/Searchbar/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from '@mui/material';
 
 const Searchbar: React.FunctionComponent = () => {
   return (
@@ -10,7 +12,12 @@ const Searchbar: React.FunctionComponent = () => {
         className={styles.search__input}
         type="text"
         placeholder="Search"
+        required={true}
+        pattern="\S+.*"
       />
+      <IconButton>
+        <CloseIcon />
+      </IconButton>
     </div>
   );
 };
