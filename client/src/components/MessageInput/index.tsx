3a7d331c76a9c7ac4cfa30bfd4d70827645d48dc
max-tsymba <young.bezero@gmail.com>
2022-01-26 13:37:49 +0200

    message input + done 1 part frontend

--- client/src/components/MessageInput/index.tsx
+++ client/src/components/MessageInput/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './styles.module.scss';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import { IconButton } from '@mui/material';
 import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 import SendIcon from '@mui/icons-material/Send';
+import autosize from 'autosize';
 
 const MessageInput = () => {
+  const refTextarea: React.RefObject<HTMLTextAreaElement> =
+    useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (refTextarea.current) {
+      refTextarea.current.focus();
+      autosize(refTextarea.current);
+    }
+  }, [refTextarea]);
+
   return (
     <div className={styles.wrapper}>
       <IconButton>
         <AttachFileIcon className={styles.attachFile__svg} />
       </IconButton>
-      <textarea className={styles.textarea} />
+      <textarea
+        placeholder="Write a message..."
+        rows={1}
+        className={styles.textarea}
+        defaultValue={''}
+        ref={refTextarea}
+      />
       <IconButton>
         <SentimentSatisfiedAltIcon />
       </IconButton>
